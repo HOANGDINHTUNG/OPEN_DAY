cbc39172d214823e1916a6b5ebdf50f56da08e3a
unknown <mtung3365@gmail.com>
2026-07-07 22:36:03 +0700

    fixx01

--- fix.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-let f = "src/features/leaderboard/LeaderboardPage.tsx";
-let c = fs.readFileSync(f, "utf8");
-c = c.replace(
-  'ChevronLeft,\n} from "lucide-react";',
-  'ChevronLeft,\n  Trophy,\n} from "lucide-react";',
-);
-fs.writeFileSync(f, c);
